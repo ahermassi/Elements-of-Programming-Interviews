11d96255e7728293152a429d253fd6668820e67a
Anouer Hermassi <ahermass@stevens.edu>
2019-06-06 17:30:55 +0100

    Basic API for linked list

---
+++ linked_lists/linked_list.py
@@ -0,0 +1,25 @@
+def search_list(lst, key):
+    """
+    Search the linked list for a given value
+    :param lst: list to search
+    :param key: value to look up
+    :return: node that holds the sought key
+    """
+    while lst and lst.data != key:
+        lst = lst.next
+    return lst  # if list is null or key not found, None is returned
+
+
+def insert_after(node, new_node):
+    """
+    Insert a new node after a specified node
+    """
+    new_node.next = node.next
+    node.next = new_node
+
+
+def deleter_after(node):
+    """
+    Delete the node past this one. Assume node is not a tail
+    """
+    node.next = node.next.next
---
+++ linked_lists/list_node.py
@@ -0,0 +1,6 @@
+class ListNode:
+
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next = next_node
+        
